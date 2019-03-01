210aea4d6eb59ffefbe031f2601d3508a5da03fe
Roni Shabo <roni.shabo@gmail.com>
2019-03-01 15:57:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,5 +32,5 @@ def merge_data(keys, data)
   merged = []
   keys.each do |x|
     data.first.map do |k, v|
-      if x.values[0] == k then merged
+      if x.values[0] == k then merged << x.v
 end
