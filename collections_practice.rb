e77f875c55c4fc88d395e9de7cb3e6959ac9fce3
Roni Shabo <roni.shabo@gmail.com>
2019-03-01 16:12:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,8 +23,9 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-  
-  
+  array.uniq.each {|i| count = 0
+        array.each {|i2| if i2 == i then count += 1 end}
+        i[:count] = count}
 end
 
 
